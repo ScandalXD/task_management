fa4a4ba8addcd39dfa29c1c02c909b2d8ae31cb3
Artur Pylypenko <artur.pilipenko2018@gmail.com>
2025-01-12 22:27:31 +0100

    second commit

--- app/db.php
+++ app/db.php
@@ -1,14 +1,14 @@
 <?php
-$host = 'db';
+$host = 'localhost';
 $db = 'task_management';
-$user = 'user';
-$pass = 'password';
+$user = 'root';
+$pass = '';
 
 // Підключення до бази даних
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die("Помилка підключення до бази даних: " . $e->getMessage());
 }
 ?>
